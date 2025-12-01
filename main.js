f228c4d7560d94ef0de38d1485d7ad2f1eff8c52
yasamanat <ataeeyasaman1@gmil.com>
2025-12-01 18:40:47 +0330

    first commit

---
+++ main.js
@@ -0,0 +1,7 @@
+let but = document.querySelector(".button")
+let pop = document.querySelector(".container")
+console.log(but);
+
+but.addEventListener("click" , function(){
+    pop.style.display = "none" ;
+})
